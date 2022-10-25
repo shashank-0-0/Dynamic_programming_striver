0bf7124b3a0bbabd2e3a500dafbe414bc72f584f
Shashank <ss0648467@gmail.com>
2022-10-25 10:58:00 +0530

    code

---
+++ k_subsetsumk_11.java
@@ -0,0 +1,40 @@
+public class k_subsetsumk_11 {
+
+      return solve(0,n,arr,k,dp);
+
+      //TABULATION (represented 'cur_ind' states by i anf 'k' states as j)
+      boolean[][] dp =new boolean[n+1][k+1];
+      for(int i=0;i<n+1;i++){
+          dp[i][0]=true;
+      }
+      for(int i=n-1;i>=0;i--){
+          for(int j=k;j>=0;j--){
+              if(arr[i]<=j){
+                  dp[i][j]=dp[i+1][j-arr[i]] || dp[i+1][j];
+              }else{
+                  dp[i][j]=dp[i+1][j];
+              }
+          }
+      }
+      //initial recursion call
+      return dp[0][k];
+
+}
+    private static boolean solve(int cur_ind,int n,int[] arr,int k,boolean[][] dp){
+
+        if(k==0){
+            return true;
+        }
+        if(cur_ind==n){
+            return false;
+        }
+        if(dp[cur_ind][k]!=-1){
+            return dp[cu
+        }
+        if(arr[cur_ind]<=k){
+            return solve(cur_ind+1,n,arr,k-arr[cur_ind],dp) || solve(cur_ind+1,n,arr,k,dp);
+        }else{
+            return solve(cur_ind+1,n,arr,k,dp);
+        }
+    }
+}
