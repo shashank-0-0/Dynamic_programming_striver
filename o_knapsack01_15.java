9786bc80ab87d4332caacbe06129165e1fb473ad
Shashank <ss0648467@gmail.com>
2022-10-25 18:07:03 +0530

    code

---
+++ o_knapsack01_15.java
@@ -0,0 +1,43 @@
+public class o_knapsack01_15 {
+    static int knapsack(int[] weight, int[] value, int n, int maxWeight) {
+
+        int[][] dp=new int[n+1][maxWeight+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        return solve(weight,value,0,maxWeight,n,dp);
+        //TABULATION
+        int[][] dp=new int[n+1][maxWeight+1];
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=maxWeight;j>=0;j--){
+                if(weight[i]<=j){
+                    int take=value[i]+dp[i+1][j-weight[i]];
+                    int nottake= dp[i+1][j];
+                    dp[i][j]=Math.max(take,nottake);
+                }else{
+                    dp[i][j]=dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][maxWeight];
+
+
+    }
+    private static int solve(int[] weight,int[] value,int cur,int maxWeight,int n,int[][] dp){
+        if(cur==n || maxWeight==0){
+            return 0;
+        }
+        if(dp[cur][maxWeight]!=-1){
+            return dp[cur][maxWeight];
+        }
+        if(weight[cur]<=maxWeight){
+            int take=value[cur]+solve(weight,value,cur+1,maxWeight-weight[cur],n,dp);
+            int nottake= solve(weight,value,cur+1,maxWeight,n,dp);
+            return dp[cur][maxWeight]=Math.max(take,nottake);
+        }else{
+            return dp[cur][maxWeight]=solve(weight,value,cur+1,maxWeight,n,dp);
+        }
+
+    }
+}
